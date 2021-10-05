74c13d4186cd17971e4245c4423c6de6e2c90afe
dario290799 <72132972+dario290799@users.noreply.github.com>
2021-10-05 12:59:36 +0200

    Actualización

--- _incrementar_05-10-21/src/main/java/com/example/_incrementar_051021/HelloController.java
+++ _incrementar_05-10-21/src/main/java/com/example/_incrementar_051021/HelloController.java
@@ -1,14 +1,44 @@
 package com.example._incrementar_051021;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TextField Result;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button restar;
+    @FXML
+    private Button sumar;
+
+    int resultado;
+
+    public void Sumar(ActionEvent actionEvent){
+
+        resultado = Integer.parseInt(Result.getText());
+
+        Result.setText(Integer.toString(resultado+1));
+
+    }
+
+    public void Restar(ActionEvent actionEvent){
+
+        resultado = Integer.parseInt(Result.getText());
+
+        Result.setText(Integer.toString(resultado-1));
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Result.setText("0");
     }
 }
