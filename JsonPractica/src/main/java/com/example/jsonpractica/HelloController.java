6b01b91d182f39a5b6e7cfb0bb2e3158238b30b5
dario290799 <72132972+dario290799@users.noreply.github.com>
2021-11-30 21:22:17 +0100

    actualizacion

--- JsonPractica/src/main/java/com/example/jsonpractica/HelloController.java
+++ JsonPractica/src/main/java/com/example/jsonpractica/HelloController.java
@@ -13,7 +13,7 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
